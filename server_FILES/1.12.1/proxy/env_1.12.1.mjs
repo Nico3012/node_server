33b5e6b65acb018b1403336d48b20de82753a2a2
Nico3012 <119159464+Nico3012@users.noreply.github.com>
2023-07-26 14:41:12 +0200

    Create env_1.12.1.mjs

---
+++ server_FILES/1.12.1/proxy/env_1.12.1.mjs
@@ -0,0 +1,7 @@
+export default {
+    cert: "../certs/localhost.crt",
+    key: "../certs/localhost.key",
+    entries: {
+        "localhost": "127.0.0.1:8080" // only local addresses for security reasons
+    }
+};
