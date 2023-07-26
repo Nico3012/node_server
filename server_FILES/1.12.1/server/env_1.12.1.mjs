53b65f75df7a4af91122b2dc7c52e574655364c7
Nico3012 <119159464+Nico3012@users.noreply.github.com>
2023-07-26 14:41:43 +0200

    Update env_1.12.1.mjs

--- server_FILES/1.12.1/server/env_1.12.1.mjs
+++ server_FILES/1.12.1/server/env_1.12.1.mjs
@@ -1,8 +1,8 @@
 export default {
     secure: false,
     // if secure
-    cert: "../certs/localhost.crt",
-    key: "../certs/localhost.key",
+    cert: "./localhost.crt",
+    key: "./localhost.key",
     // if !secure
     port: 8080
 };
