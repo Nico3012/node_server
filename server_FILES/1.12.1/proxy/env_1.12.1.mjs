f462b7d9af0abe721bb1c671a5b59d34cdc6587c
Nico3012 <119159464+Nico3012@users.noreply.github.com>
2023-09-26 13:20:05 +0200

    Delete server_FILES/1.12.1 directory

--- server_FILES/1.12.1/proxy/env_1.12.1.mjs
+++
@@ -1,7 +0,0 @@
-export default {
-    cert: "./localhost.crt",
-    key: "./localhost.key",
-    entries: {
-        "localhost": "127.0.0.1:8080" // only local addresses for security reasons
-    }
-};
--- server_FILES/1.12.1/server/env_1.12.1.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-    secure: true,
-    // if secure
-    cert: "./localhost.crt",
-    key: "./localhost.key",
-    // if !secure
-    port: 8080
-};
