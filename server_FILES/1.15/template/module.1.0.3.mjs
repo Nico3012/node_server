23c9ae60a11a350d2881e4a9c21786a5a54e9427
Nico3012 <119159464+Nico3012@users.noreply.github.com>
2023-09-26 11:13:53 +0200

    Create module.1.0.3.mjs

---
+++ server_FILES/1.15/template/module.1.0.3.mjs
@@ -0,0 +1,10 @@
+// create __filename variable in module
+let { fileURLToPath, pathToFileURL } = await import("node:url"); // use pathToFileURL for dynamic import
+let __filename = fileURLToPath(import.meta.url);
+
+// create __dirname variable in module
+let { dirname, join } = await import("node:path");
+let __dirname = dirname(__filename);
+
+// let { x, y, z } = await import(pathToFileURL(join(__dirname, "..", "dir", "file.mjs")).href); // import x, y, z from "../dir/file.mjs"
+// let stats = stat(join(__dirname, "..", "dir", "file.ext")); // get stats from "../dir/file.ext"
